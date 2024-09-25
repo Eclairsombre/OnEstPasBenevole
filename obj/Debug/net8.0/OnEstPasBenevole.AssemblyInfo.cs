708d3140ce8a5dfd03bd98dc0760c0684a50ee6d
Eclairsombre <130174141+Eclairsombre@users.noreply.github.com>
2024-09-25 10:34:25 +0100

    fix new release

--- obj/Debug/net8.0/OnEstPasBenevole.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnEstPasBenevole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd7d1c4124afb42e68421185c7c08da4beb311d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf4bd298762c6839f72148cd65069c250ae620a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/linux-x64/OnEstPasBenevole.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/OnEstPasBenevole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7e3e9ae9f65bdffe19e3812965274cf9e917ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf4bd298762c6839f72148cd65069c250ae620a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnEstPasBenevole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
